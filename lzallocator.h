4d7df877c44f72b845dd8a351778395b2be345f7
ByteGuy8 <>
2024-06-18 20:14:04 -0400

    Now headers consume portion of the blocks space in the allocator

--- lzallocator.h
+++ lzallocator.h
@@ -7,10 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef LZALLOCATOR_BLOCK_SIZE
-#define LZALLOCATOR_BLOCK_SIZE 4
-#endif
-
 typedef struct _lzallocator_header_
 {
     char free;
@@ -21,10 +17,12 @@ typedef struct _lzallocator_header_
 
 typedef struct _lzallocator_
 {
-    size_t asize;
-    size_t bsize;
+    size_t bytes;
     struct _lzallocator_header_ *blocks;
 } LZAllocator;
+
+#define LZALLOCATOR_HEADER_SIZE (sizeof(struct _lzallocator_header_))
+#define LZALLOCATOR_CALC_BLOCK_SIZE(size) (size - LZALLOCATOR_HEADER_SIZE)
 
 int lzallocator_is_power_of_two(size_t value)
 {
@@ -74,18 +72,18 @@ void lzallocator_join_blocks(struct _lzallocator_ *allocator)
 
 struct _lzallocator_header_ *lzallocator_split_block(struct _lzallocator_header_ *header)
 {
-    if (header->size / 2 < LZALLOCATOR_BLOCK_SIZE)
+    size_t header_size = sizeof(struct _lzallocator_header_);
+
+    // A block size can be less or equals to the header side.
+    // Blocks need to have enough space for headers, which contains
+    // information of that block: free, previous o next.
+    if (header->size / 2 <= header_size)
         return NULL;
 
     header->size /= 2;
 
-    size_t header_size = sizeof(struct _lzallocator_header_);
-    size_t blocks_count = header->size / LZALLOCATOR_BLOCK_SIZE;
-    size_t headers_size = header_size * blocks_count;
-    size_t total_size = headers_size + header->size;
-
     unsigned char *area = (unsigned char *)header;
-    unsigned char *new_area = area + total_size;
+    unsigned char *new_area = area + header->size;
 
     struct _lzallocator_header_ *next = (struct _lzallocator_header_ *)new_area;
 
@@ -109,44 +107,37 @@ void *lzallocator_alloc_from_header(struct _lzallocator_header_ *header)
 
     header->free = 0;
 
-    return (void *)(((unsigned char *)header) + sizeof(struct _lzallocator_header_));
+    return (void *)(((unsigned char *)header) + LZALLOCATOR_HEADER_SIZE);
 }
 
 struct _lzallocator_header_ *lzallocator_get_header(void *ptr)
 {
-    return (struct _lzallocator_header_ *)(((unsigned char *)ptr) - sizeof(struct _lzallocator_header_));
+    return (struct _lzallocator_header_ *)(((unsigned char *)ptr) - LZALLOCATOR_HEADER_SIZE);
 }
 
-size_t lzallocator_kib_count(size_t kb_size)
+size_t lzallocator_kib(size_t kb_size)
 {
     size_t modulo = kb_size % 2;
     size_t padding = 2 - modulo;
     size_t size = modulo == 0 ? kb_size : kb_size + padding;
 
-    return size * 1024 / LZALLOCATOR_BLOCK_SIZE;
+    return size * 1024;
 }
 
-size_t lzallocator_mib_count(size_t mb_size)
+size_t lzallocator_mib(size_t mb_size)
 {
     size_t modulo = mb_size % 2;
     size_t padding = 2 - modulo;
     size_t size = modulo == 0 ? mb_size : mb_size + padding;
 
-    return size * (1024 * 1024) / LZALLOCATOR_BLOCK_SIZE;
+    return size * (1024 * 1024);
 }
 
-struct _lzallocator_ *lzallocator_create(size_t block_count)
+struct _lzallocator_ *lzallocator_create(size_t bytes)
 {
-    assert(lzallocator_is_power_of_two(LZALLOCATOR_BLOCK_SIZE && "Illegal block size. Not power of two"));
-    assert(lzallocator_is_power_of_two(block_count) && "Illegal block count. Not power of two");
+    assert(lzallocator_is_power_of_two(bytes) && "Illegal bytes value. Not power of two");
 
-    size_t block_size = LZALLOCATOR_BLOCK_SIZE;
-    size_t blocks_size = block_size * block_count;
-
-    size_t header_size = sizeof(struct _lzallocator_header_);
-    size_t headers_size = header_size * block_count;
-
-    void *area = malloc(headers_size + blocks_size);
+    void *area = malloc(bytes);
     struct _lzallocator_ *allocator = (struct _lzallocator_ *)malloc(sizeof(struct _lzallocator_));
 
     if (!area || !allocator)
@@ -157,31 +148,18 @@ struct _lzallocator_ *lzallocator_create(size_t block_count)
         return NULL;
     }
 
-    allocator->asize = headers_size + blocks_size;
-    allocator->bsize = blocks_size;
+    allocator->bytes = bytes;
     allocator->blocks = (struct _lzallocator_header_ *)area;
 
     allocator->blocks->free = 1;
-    allocator->blocks->size = blocks_size;
+    allocator->blocks->size = bytes;
     allocator->blocks->prev = NULL;
     allocator->blocks->next = NULL;
 
     return allocator;
 }
 
-size_t lzallocator_calculate_bytes(size_t size)
-{
-    size_t bytes = size / LZALLOCATOR_BLOCK_SIZE;
-
-    if (size % LZALLOCATOR_BLOCK_SIZE != 0)
-        bytes += 1;
-
-    bytes *= LZALLOCATOR_BLOCK_SIZE;
-
-    return bytes;
-}
-
-struct _lzallocator_header_ *lzallocator_best_fit(size_t size, size_t bytes, struct _lzallocator_ *allocator)
+struct _lzallocator_header_ *lzallocator_best_fit(size_t bytes, struct _lzallocator_ *allocator)
 {
     size_t last_size = 0;
     struct _lzallocator_header_ *header = NULL;
@@ -195,16 +173,23 @@ struct _lzallocator_header_ *lzallocator_best_fit(size_t size, size_t bytes, str
         // contains the requested size. But we need to try the best fit. A very large
         // block would be a waste of (maybe) space and execution time, because it will
         // need to be splitted later. So we check to find the at lest large enough block
-        if (current->free && (current->size >= bytes && (current->size < last_size || last_size == 0)))
+
+        size_t current_size = LZALLOCATOR_CALC_BLOCK_SIZE(current->size);
+
+        if (current_size == bytes)
+            break;
+
+        if (current->free && (current_size > bytes && (current_size < last_size || last_size == 0)))
         {
             header = current;
-            last_size = current->size;
+            last_size = current_size;
             size_t fit = last_size / bytes;
 
             // if the found block is large enough to
             // contain at least one or two times the
             // requested size, we break. We found the best correct block
-            if (fit == 1 && fit < 2)
+
+            if (fit >= 1 && fit <= 2)
                 break;
         }
 
@@ -214,38 +199,36 @@ struct _lzallocator_header_ *lzallocator_best_fit(size_t size, size_t bytes, str
     return header;
 }
 
-struct _lzallocator_header_ *lzallocator_find_by_split(size_t size, size_t bytes, struct _lzallocator_header_ *header)
+struct _lzallocator_header_ *lzallocator_find_by_split(size_t bytes, struct _lzallocator_header_ *header)
 {
     assert(header && "header is NULL");
 
-    if (header->free == 0)
+    size_t block_size = LZALLOCATOR_CALC_BLOCK_SIZE(header->size);
+
+    if (!header->free)
         return NULL;
-    if (header->size <= LZALLOCATOR_BLOCK_SIZE)
-        return NULL;
-    if (bytes > header->size)
+    if (bytes > block_size)
         return NULL;
 
-    if (bytes == header->size)
+    if (bytes == block_size)
         return header;
-    if (header->size / 2 < bytes)
+    if (LZALLOCATOR_CALC_BLOCK_SIZE(header->size / 2) < bytes)
         return header;
 
     do
     {
         lzallocator_split_block(header);
-    } while (header->size / 2 >= bytes);
+    } while (LZALLOCATOR_CALC_BLOCK_SIZE(header->size / 2) >= bytes);
 
     return header;
 }
 
-void *lzallocator_alloc(size_t size, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
+void *lzallocator_alloc(size_t bytes, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
 {
-    size_t bytes = lzallocator_calculate_bytes(size);
-
     // If the first block was splitted, we need to check for every block to find the best for the request size
     if (allocator->blocks->next)
     {
-        struct _lzallocator_header_ *header = lzallocator_best_fit(size, bytes, allocator);
+        struct _lzallocator_header_ *header = lzallocator_best_fit(bytes, allocator);
 
         if (!header)
             return NULL;
@@ -253,7 +236,7 @@ void *lzallocator_alloc(size_t size, struct _lzallocator_ *allocator, struct _lz
         // If the found block size is bigger enough to contains the requested size twice, then we
         // split the block to give the best block size possible
         if (header->size / 2 >= bytes)
-            header = lzallocator_find_by_split(size, bytes, header);
+            header = lzallocator_find_by_split(bytes, header);
 
         if (header_out)
             *header_out = header;
@@ -265,7 +248,7 @@ void *lzallocator_alloc(size_t size, struct _lzallocator_ *allocator, struct _lz
         return ptr;
     }
 
-    struct _lzallocator_header_ *header = lzallocator_find_by_split(size, bytes, allocator->blocks);
+    struct _lzallocator_header_ *header = lzallocator_find_by_split(bytes, allocator->blocks);
 
     if (!header)
         return NULL;
@@ -280,12 +263,12 @@ void *lzallocator_alloc(size_t size, struct _lzallocator_ *allocator, struct _lz
     return ptr;
 }
 
-void *lzallocator_malloc(size_t size, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
+void *lzallocator_malloc(size_t bytes, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
 {
-    void *ptr = lzallocator_alloc(size, allocator, header_out);
+    void *ptr = lzallocator_alloc(bytes, allocator, header_out);
 
     if (ptr)
-        memset(ptr, 0, size);
+        memset(ptr, 0, bytes);
 
     return ptr;
 }
@@ -294,7 +277,7 @@ int lzallocator_validate_ptr(void *ptr, struct _lzallocator_ *allocator)
 {
     unsigned char *block_ptr = (unsigned char *)ptr;
 
-    return (block_ptr < (unsigned char *)allocator->blocks) || (block_ptr > (((unsigned char *)allocator->blocks) + allocator->asize));
+    return (block_ptr < (unsigned char *)allocator->blocks) || (block_ptr > (((unsigned char *)allocator->blocks) + allocator->bytes));
 }
 
 void lzallocator_dealloc(void *ptr, struct _lzallocator_ *allocator)
@@ -309,15 +292,15 @@ void lzallocator_dealloc(void *ptr, struct _lzallocator_ *allocator)
     lzallocator_join_blocks(allocator);
 }
 
-void *lzallocator_realloc(size_t size, void *ptr, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
+void *lzallocator_realloc(size_t bytes, void *ptr, struct _lzallocator_ *allocator, struct _lzallocator_header_ **header_out)
 {
     if (!ptr)
-        return lzallocator_alloc(size, allocator, header_out);
+        return lzallocator_alloc(bytes, allocator, header_out);
 
     if (lzallocator_validate_ptr(ptr, allocator))
         return NULL;
 
-    void *new_ptr = lzallocator_alloc(size, allocator, header_out);
+    void *new_ptr = lzallocator_alloc(bytes, allocator, header_out);
 
     if (new_ptr)
     {
